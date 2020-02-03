052370c116ddea20278057efb3d13416fc43dfe6
daknichu <jeongmin.choi@gmail.com>
2020-02-04 08:13:21 +0900

    jpa setting

--- src/main/java/com/epopcon/springboot/domain/posts/Posts.java
+++ src/main/java/com/epopcon/springboot/domain/posts/Posts.java
@@ -30,4 +30,6 @@ public class Posts {
         this.author = author;
     }
 
+    public void update() {
+    }
 }
---
+++ src/main/java/com/epopcon/springboot/service/PostsService.java
@@ -0,0 +1,37 @@
+package com.epopcon.springboot.service;
+
+import com.epopcon.springboot.domain.posts.Posts;
+import com.epopcon.springboot.domain.posts.PostsRepository;
+import com.epopcon.springboot.web.dto.PostsResponseDto;
+import com.epopcon.springboot.web.dto.PostsSaveRequestDto;
+import com.epopcon.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다." + id));
+
+        posts.update();
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다." + id));
+        return new PostsResponseDto(entity);
+    }
+}
